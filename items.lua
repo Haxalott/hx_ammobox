059bf79f84dc97ed84db1673353ef58d9e865e54
Haxalott's Developments <43390732+Haxalott@users.noreply.github.com>
2025-01-02 19:20:18 +1100

    Create items.lua

---
+++ items.lua
@@ -0,0 +1,20 @@
+	['ammo-9-box'] = {
+		label = '9mm Ammo Box',
+		weight = 1000,
+		stack = true,
+		close = true
+	},
+
+	['ammo-rifle-box'] = {
+		label = 'Rifle Ammo Box',
+		weight = 5000,
+		stack = true,
+		close = true
+	},
+
+	['ammo-shotgun-box'] = {
+		label = 'Shotgun Ammo Box',
+		weight = 7500,
+		stack = true,
+		close = true
+	},
